4a8f5f6096f416d7cddc478a2ffefe45e1be2139
Kuba Kmiotek <kuba.kmiotek@snowflake.com>
2023-12-13 17:09:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-st.text('fail')
+st.text('fail2')
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
